0e718f527f7b8c6e392f9d99c6e6df9a9fa13e24
kharygueye05 <mamakharygueye4@gmail.com>
2025-12-16 22:44:10 +0000

    Initialisation et déploiement

---
+++ gescommandeCsharp/gescommandeCsharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gescommandeCsharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
